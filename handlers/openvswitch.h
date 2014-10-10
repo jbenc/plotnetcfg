79a7bce3cedb17c747c167943b6d10f0ece494d7
Jiri Benc <jbenc@redhat.com>
2014-10-10 13:14:04 +0200

    Add licence header to files
    Mainly to clarify that the code is GPLv2+.
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>

--- handlers/openvswitch.h
+++ handlers/openvswitch.h
@@ -1,3 +1,18 @@
+/*
+ * This file is a part of plotnetcfg, a tool to visualize network config.
+ * Copyright (C) 2014 Red Hat, Inc. -- Jiri Benc <jbenc@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #ifndef _HANDLERS_OPENVSWITCH_H
 #define _HANDLERS_OPENVSWITCH_H
 
