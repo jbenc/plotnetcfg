jbenc/plotnetcfg

commit ae3278386df7dbe26e54d5ba654dba7e8caffdcd

bond: new handler
Bonding handler currently supports reading mode and active slave.

When appropriate, non-active slaves have flag IF_PASSIVE_SLAVE and are
denoted by dashed arrow in dot output.

Signed-off-by: Ondrej Hlavaty <ohlavaty@redhat.com>

handlers/bond.c
@@ -1,0 +1,184 @@
+/*
+ * This file is a part of plotnetcfg, a tool to visualize network config.
+ * Copyright (C) 2016 Red Hat, Inc. -- Ondrej Hlavaty <ohlavaty@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#define _GNU_SOURCE
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <strings.h>
+#include "../handler.h"
+#include "../sysfs.h"
+#include "../label.h"
+#include "../netlink.h"
+#include "../utils.h"
+#include "bond.h"
+
+#include "../compat.h"
+
+static const char *bond_mode_name[] = {
+	"",
+	"balance-rr",
+	"active-backup",
+	"balance-xor",
+	"broadcast",
+	"802.3ad",
+	"balance-tlb",
+	"balance-alb",
+};
+
+static int bond_netlink(struct if_entry *entry, struct rtattr **tb);
+static int bond_scan(struct if_entry *entry);
+static int bond_post(struct if_entry *entry, struct netns_entry *root);
+static void bond_cleanup(struct if_entry *entry);
+
+static struct handler h_bond = {
+	.driver = "bonding",
+	.netlink = bond_netlink,
+	.scan = bond_scan,
+	.post = bond_post,
+	.cleanup = bond_cleanup,
+};
+
+struct bond_private {
+	uint8_t mode;
+	unsigned int active_slave_index;
+	char *active_slave_name;
+};
+
+void handler_bond_register(void)
+{
+	handler_register(&h_bond);
+}
+
+static int bond_netlink(struct if_entry *entry, struct rtattr **tb)
+{
+	struct bond_private *priv;
+	struct rtattr *linkinfo[IFLA_INFO_MAX + 1];
+	struct rtattr *bondinfo[IFLA_BOND_MAX + 1];
+
+	priv = calloc(sizeof(*priv), 1);
+	if (!priv)
+		return ENOMEM;
+	entry->handler_private = priv;
+
+	if (!tb[IFLA_LINKINFO])
+		return ENOENT;
+	rtnl_parse_nested(linkinfo, IFLA_INFO_MAX, tb[IFLA_LINKINFO]);
+
+	if (!linkinfo[IFLA_INFO_DATA])
+		return ENOENT;
+	rtnl_parse_nested(bondinfo, IFLA_BOND_MAX, linkinfo[IFLA_INFO_DATA]);
+
+	if (bondinfo[IFLA_BOND_MODE]) {
+		priv->mode = *(uint8_t *)RTA_DATA(bondinfo[IFLA_BOND_MODE]) + 1;
+		if (priv->mode > ARRAY_SIZE(bond_mode_name))
+			priv->mode = 0;
+	}
+
+	if (bondinfo[IFLA_BOND_ACTIVE_SLAVE]) {
+		priv->active_slave_index = *(uint32_t *)RTA_DATA(bondinfo[IFLA_BOND_ACTIVE_SLAVE]);
+	}
+
+	return 0;
+}
+
+static ssize_t bond_get_sysfs(char **dest, struct if_entry *entry, const char *prop)
+{
+	char *path;
+	ssize_t len;
+
+	*dest = NULL;
+	if (asprintf(&path, "class/net/%s/bonding/%s", entry->if_name, prop) < 0)
+		return -errno;
+
+	len = sysfs_readfile(dest, path);
+	free(path);
+	if (len < 0) {
+		if (len == -ENOENT)
+			return 0;
+		return len;
+	}
+
+	if (len == 0)
+		free(dest);
+
+	return len;
+}
+
+static int match_active_slave(struct if_entry *entry, struct if_entry *master)
+{
+	struct bond_private *priv = master->handler_private;
+
+	if (priv->active_slave_index && entry->if_index != priv->active_slave_index)
+		return 0;
+	if (priv->active_slave_name && strcmp(entry->if_name, priv->active_slave_name))
+		return 0;
+	return 1;
+}
+
+static int bond_scan(struct if_entry *entry)
+{
+	char *dest, *tmp;
+	struct bond_private *priv = entry->handler_private;
+
+	if (!priv->mode) {
+		if (bond_get_sysfs(&dest, entry, "mode") > 0) {
+			tmp = index(dest, ' ');
+			if (tmp)
+				priv->mode = atoi(tmp + 1) + 1;
+			free(dest);
+		}
+		if (priv->mode > ARRAY_SIZE(bond_mode_name))
+			priv->mode = 0;
+	}
+
+	if (!priv->active_slave_index)
+		bond_get_sysfs(&priv->active_slave_name, entry, "active_slave");
+
+	return 0;
+}
+static int bond_post(struct if_entry *entry, _unused struct netns_entry *root)
+{
+	struct bond_private *priv = entry->handler_private;
+	struct if_list_entry *ile;
+
+	if (priv->mode && *bond_mode_name[priv->mode])
+		label_add(&entry->label, "mode: %s", bond_mode_name[priv->mode]);
+
+	if (priv->active_slave_index || priv->active_slave_name) {
+		for (ile = entry->rev_master; ile; ile = ile->next) {
+			if (match_active_slave(ile->entry, entry)) {
+				entry->active_slave = ile->entry;
+				label_add(&entry->label, "active slave: %s", entry->active_slave->if_name);
+			} else {
+				ile->entry->flags |= IF_PASSIVE_SLAVE;
+			}
+		}
+	}
+	return 0;
+}
+
+static void bond_cleanup(struct if_entry *entry)
+{
+	struct bond_private *priv = entry->handler_private;
+
+	if (priv) {
+		if (priv->active_slave_name)
+			free(priv->active_slave_name);
+		free(entry->handler_private);
+	}
+}

Makefile
@@ -8,7 +8,7 @@ endif
 CFLAGS=-W -Wall $(INCLUDE) $(EXTRA_CFLAGS)
 
 OBJECTS=args ethtool frontend handler if label main match netlink netns tunnel utils sysfs master
-HANDLERS=bridge openvswitch veth vlan iov
+HANDLERS=bridge openvswitch veth vlan iov bond
 FRONTENDS=dot json
 
 OBJ=$(OBJECTS:%=%.o) $(HANDLERS:%=handlers/%.o) $(FRONTENDS:%=frontends/%.o)

compat.h
@@ -53,4 +53,35 @@ struct ovs_header {
 	int dp_ifindex;
 };
 
+#ifndef IFLA_BOND_MAX
+enum {
+	IFLA_BOND_UNSPEC,
+	IFLA_BOND_MODE,
+	IFLA_BOND_ACTIVE_SLAVE,
+	IFLA_BOND_MIIMON,
+	IFLA_BOND_UPDELAY,
+	IFLA_BOND_DOWNDELAY,
+	IFLA_BOND_USE_CARRIER,
+	IFLA_BOND_ARP_INTERVAL,
+	IFLA_BOND_ARP_IP_TARGET,
+	IFLA_BOND_ARP_VALIDATE,
+	IFLA_BOND_ARP_ALL_TARGETS,
+	IFLA_BOND_PRIMARY,
+	IFLA_BOND_PRIMARY_RESELECT,
+	IFLA_BOND_FAIL_OVER_MAC,
+	IFLA_BOND_XMIT_HASH_POLICY,
+	IFLA_BOND_RESEND_IGMP,
+	IFLA_BOND_NUM_PEER_NOTIF,
+	IFLA_BOND_ALL_SLAVES_ACTIVE,
+	IFLA_BOND_MIN_LINKS,
+	IFLA_BOND_LP_INTERVAL,
+	IFLA_BOND_PACKETS_PER_SLAVE,
+	IFLA_BOND_AD_LACP_RATE,
+	IFLA_BOND_AD_SELECT,
+	IFLA_BOND_AD_INFO,
+	__IFLA_BOND_MAX,
+};
+#define IFLA_BOND_MAX	(__IFLA_BOND_MAX - 1)
+#endif
+
 #endif

frontends/dot.c
@@ -83,10 +83,11 @@ static void output_ifaces_pass2(struct if_entry *list)
 	for (ptr = list; ptr; ptr = ptr->next) {
 		if (ptr->master) {
 			printf("\"%s\" -> ", ifid(ptr));
-			printf("\"%s\"", ifid(ptr->master));
+			printf("\"%s\" [style=%s", ifid(ptr->master),
+			       ptr->flags & IF_PASSIVE_SLAVE ? "dashed" : "solid");
 			if (ptr->edge_label && !ptr->link)
-				printf(" [label=\"%s\"]", ptr->edge_label);
-			printf("\n");
+				printf(",label=\"%s\"", ptr->edge_label);
+			printf("]\n");
 		}
 		if (ptr->physfn) {
 			printf("\"%s\" -> ", ifid(ptr));

handlers/bond.h
@@ -1,0 +1,21 @@
+/*
+ * This file is a part of plotnetcfg, a tool to visualize network config.
+ * Copyright (C) 2016 Red Hat, Inc. -- Ondrej Hlavaty <ohlavaty@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _HANDLERS_BOND_H
+#define _HANDLERS_BOND_H
+
+void handler_bond_register(void);
+
+#endif

if.h
@@ -49,6 +49,7 @@ struct if_entry {
 	struct label *label;
 	unsigned int master_index;
 	struct if_entry *master;
+	struct if_entry *active_slave;
 	unsigned int link_index;
 	int link_netnsid;
 	struct if_entry *link;
@@ -67,11 +68,12 @@ struct if_entry {
 	struct if_entry *physfn;
 };
 
-#define IF_LOOPBACK	1
-#define IF_UP		2
-#define IF_HAS_LINK	4
-#define IF_INTERNAL	8
-#define IF_PEER_WEAK	16
+#define IF_LOOPBACK		1
+#define IF_UP			2
+#define IF_HAS_LINK		4
+#define IF_INTERNAL		8
+#define IF_PEER_WEAK		16
+#define IF_PASSIVE_SLAVE	32
 
 int if_list(struct if_entry **result, struct netns_entry *ns);
 void if_list_free(struct if_entry *list);

main.c
@@ -34,6 +34,7 @@
 #include "handlers/veth.h"
 #include "handlers/vlan.h"
 #include "handlers/iov.h"
+#include "handlers/bond.h"
 
 static void register_frontends(void)
 {
@@ -49,6 +50,7 @@ static void register_handlers(void)
 	handler_vlan_register();
 	handler_bridge_register();
 	handler_iov_register();
+	handler_bond_register();
 }
 
 static int print_help(_unused char *arg)

commit bb55bb94d7b01997487f80d5c010a9acc165f6ca

bond: fix potential out of bounds access
Fix off by one in bond_mode_name array access.

Fixes: ae3278386df7 ("bond: new handler")
Signed-off-by: Jiri Benc <jbenc@redhat.com>

handlers/bond.c
@@ -77,7 +77,7 @@ static int bond_netlink(struct if_entry *entry, struct nlattr **linkinfo)
 
 	if (bondinfo[IFLA_BOND_MODE]) {
 		priv->mode = nla_read_u8(bondinfo[IFLA_BOND_MODE]) + 1;
-		if (priv->mode > ARRAY_SIZE(bond_mode_name))
+		if (priv->mode >= ARRAY_SIZE(bond_mode_name))
 			priv->mode = 0;
 	}
 
@@ -135,7 +135,7 @@ static int bond_scan(struct if_entry *entry)
 				priv->mode = atoi(tmp + 1) + 1;
 			free(dest);
 		}
-		if (priv->mode > ARRAY_SIZE(bond_mode_name))
+		if (priv->mode >= ARRAY_SIZE(bond_mode_name))
 			priv->mode = 0;
 	}
 